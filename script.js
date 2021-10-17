0ccc31da0b7ff8aa64f01c89c410c5e76185ad44
Fishball Yuen <oxfishballxo@gmail.com>
2021-10-17 17:53:37 +0800

    initial version

---
+++ script.js
@@ -0,0 +1,38 @@
+function askGrid(){
+    let grid = prompt('how many grid you want')
+    let gridNumber=grid*grid
+    if(grid>100||grid<1){
+        alert('not valid number!')
+    }else{
+        creategrid(gridNumber)
+        document.getElementById('container').style.gridTemplateColumns=(`repeat(${grid}, 1fr)`)
+        console.log('selected')
+    }
+}
+
+function creategrid(gridNumber){
+    for (i=0;i<gridNumber;i++){
+        var div = document.createElement('div');
+        var contain = document.getElementById('container');
+        div.classList.add('div2')
+        div.setAttribute('id',i)
+        contain.appendChild(div);
+        div.addEventListener('mouseover',event=>{event.target.style.backgroundColor='blue'})
+    }
+}
+
+const resetButton=document.getElementById('reset').addEventListener("click",resetContainer,false)
+
+function resetContainer(){
+    const contain = document.getElementById('container');
+    const divs = document.getElementsByClassName('div2');
+    while(divs.length){
+        for (i=0;i<divs.length;i++){
+            contain.removeChild(divs[i])
+        }
+    }
+    console.log('reset!',divs)
+    askGrid()
+}
+
+askGrid();
